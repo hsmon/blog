715fc1b5e5fb9d979f66b6a8f9820b7135675c76
hsmon <hso92.bb@gmail.com>
2021-01-16 13:22:03 +0900

    fix: ダークモードのスタイルをstyles.cssへ移行

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
+import React from "react"
 import { AppProps } from "next/app"
 import "../styles/styles.css"
 import Header from '../components/Header'
+import { changeDarkmodeTheme } from "../functions/changeDarkMode"
 
 function App({ Component, pageProps }: AppProps) {
+  // 初期化
+  changeDarkmodeTheme()
   return (
-    <div className="bg-white dark:bg-gray-800 transition-all duration-500">
+    <div className="wrapper">
       <div className="max-w-screen-lg mx-auto px-5 min-h-screen">
         <Header />
         <main>
