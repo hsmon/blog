11e1aafcf33ad412e6b1863d6582101657665cad
hsmon <hso92.bb@gmail.com>
2021-01-17 11:28:13 +0900

    tailwind cssのeslintを追加

--- components/Thumb.tsx
+++ components/Thumb.tsx
@@ -3,29 +3,31 @@ import { Contents } from "../types/blog"
 import React from "react"
 import moment from "moment"
 
-const Thumb = ({blog}:{blog:Contents}) => {
+const Thumb = ({ blog }: { blog: Contents }) => {
   return (
-    <figure className="thumb-figure h-full rounded-lg overflow-hidden shadow transition-shadow transition-transform md:hover:shadow-md md:hover:-translate-y-2">
+    <figure className="thumb-figure h-full rounded-lg overflow-hidden shadow transition-all duration-300 md:hover:shadow-md">
       <Link href="/blogs/[id]" as={`/blogs/${blog.id}`}>
         <a className="block">
-          <div className="thumb-figure__head bg-gray-300 relative overflow-hidden" style={{paddingTop: "62.5%",filter: "grayscale(1)"}}>
+          <div
+            className="thumb-figure__head bg-gray-300 relative overflow-hidden"
+            style={{ paddingTop: "62.5%", filter: "grayscale(1)" }}
+          >
             <img src={blog.thumb[0].thumb.url} width="300" height="200" />
           </div>
           <div className="thumb-figure__body">
-            <time className="thumb-figure__time">{moment(blog.createdAt).format("YYYY-MM-DD")}</time>
+            <time className="thumb-figure__time">
+              {moment(blog.createdAt).format("YYYY-MM-DD")}
+            </time>
             <h2 className="thumb-figure__caption">{blog.title}</h2>
-            {
-              !!blog.tags.length &&
-              (
-                <ul className="tag-list">
-                  {blog.tags.map((tag) => (
-                    <li key={tag.id}>
-                      <span>{tag.name}</span>
-                    </li>
-                  ))}
-                </ul>
-              )
-            }
+            {!!blog.tags.length && (
+              <ul className="tag-list">
+                {blog.tags.map((tag) => (
+                  <li key={tag.id}>
+                    <span>{tag.name}</span>
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
         </a>
       </Link>
